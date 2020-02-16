d29374da9bb8e25ed932ba8bf2fef72eaf3e7b88
SyntaxxS <syntaxxs@googlemail.com>
2020-02-16 13:54:15 +0100

    maybe_finished

--- Wörterbuch.py
+++ Wörterbuch.py
@@ -1,6 +1,7 @@
-f = open("Wörterbuch.txt", "a")
-with open("myfile.txt", "r") as s:
+f = open("Wörterbuch.txt", "a", encoding="utf-8")
+with open("quelle.txt", "r", encoding="utf-8") as s:
     for i in s:
         for word in i.split():
-            print(word.strip(",.?()/&%$=´}{[]~^°`§+#*;:«"), file=f)
+            print(word.strip("""'0123456789\",.?\(\)»/\][&%$=´}{~^°`§+#*;:«""").lower(), file=f)
 f.close()
+open("quelle.txt", "w").close()
